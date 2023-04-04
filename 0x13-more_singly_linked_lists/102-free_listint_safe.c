32bd2b87c311814420382d894e13d8bada1d8e44
Domains18 <gibsonsgibson88@gmail.com>
2023-04-04 11:22:25 -0700

    alx-driving-me-mad

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,48 +1,31 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * free_listint_safe - frees a linked list safely, avoiding infinite loops
- * @h: pointer to the first node in the linked list
+ * print_listint_safe - Prints a linked list, safely avoiding infinite loops
+ * @head: Pointer to the list of type listint_t to print
  *
- * Return: number of elements in the freed list
+ * Return: Number of nodes in the list
  */
-size_t free_listint_safe(listint_t **h)
+size_t print_listint_safe(const listint_t *head)
 {
-	size_t node_count = 0;
-	int diff;
-	listint_t *temp_node;
+	size_t num_nodes = 0;
+	long int diff;
 
-	// if the list is empty, return 0
-	if (!h || !*h)
-		return (0);
-
-	// loop through the list
-	while (*h)
+	while (head)
 	{
-		diff = *h - (*h)->next;  // difference between current and next node
+		diff = head - head->next;
+		num_nodes++;
+		printf("[%p] %d\n", (void *)head, head->n);
 
-		// if the difference is positive, move to the next node and free the current node
 		if (diff > 0)
-		{
-			temp_node = (*h)->next;
-			free(*h);
-			*h = temp_node;
-			node_count++;
-		}
-		// if the difference is zero or negative, it means we've reached a loop
-		// so free the current node and break out of the loop
+			head = head->next;
 		else
 		{
-			free(*h);
-			*h = NULL;
-			node_count++;
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
 	}
 
-	// set the head to NULL to prevent dangling pointers
-	*h = NULL;
-
-	return (node_count);
+	return (num_nodes);
 }
-

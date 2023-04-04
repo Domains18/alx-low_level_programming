d53308fd41f4832285fafee6520a8ee3cfa8efe2
Domains18 <gibsonsgibson88@gmail.com>
2023-04-04 09:49:12 -0700

    alx-driving-me-mad

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len -functuion to get the no of elements in a singly linked list
+ *
+ * @h: head of a singly linked list
+ *
+ * Return: no of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
+

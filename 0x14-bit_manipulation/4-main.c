4eb0725a317424c2d8583ade90bb8b56e66918e3
Domains18 <gibsonsgibson88@gmail.com>
2023-04-06 12:55:22 -0700

    alx-gonna-kill-me

---
+++ 0x14-bit_manipulation/4-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    unsigned long int num;
+
+    num = 1024;
+    clear_bit(&num, 10);
+    printf("%lu\n", num);
+
+    num = 0;
+    clear_bit(&num, 10);
+    printf("%lu\n", num);
+
+    num = 98;
+    clear_bit(&num, 1);
+    printf("%lu\n", num);
+
+    return 0;
+}

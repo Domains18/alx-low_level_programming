bb5e6f87abab28fbe06c9991e30ddf8e81292dc9
Domains18 <gibsonsgibson88@gmail.com>
2023-04-09 00:29:34 -0700

    alx-struggles

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,53 +1,49 @@
-//Header file inclusion
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 #include "main.h"
 
 /**
- * Function: appendTextToFile
- * --------------------------
- * Appends text at the end of a file.
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
  *
- * @param filename: Pointer to the name of the file.
- * @param text_content: The string to add to the end of the file.
- *
- * @return: -1 if function fails or filename is NULL.
- *          -1 if the file does not exist and the user lacks write permissions.
- *           1 otherwise.
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
  */
-int appendTextToFile(const char *filename, char *text_content) {
-    // File descriptor, write count and length initialization
-    int fd, wrCount, len = 0;
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_descriptor, write_status, content_length = 0;
 
-    // Check if the filename is NULL
-    if (filename == NULL) {
-        return -1;
-    }
+	if (filename == NULL)
+	{
+		fprintf(stderr, "Error: Filename is NULL\n");
+		return (-1);
+	}
 
-    // Calculate the length of the text to append
-    if (text_content != NULL) {
-        while (text_content[len]) {
-            len++;
-        }
-    }
+	if (text_content != NULL)
+	{
+		content_length = strlen(text_content);
+	}
 
-    // Open the file with write only and append mode
-    fd = open(filename, O_WRONLY | O_APPEND);
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	if (file_descriptor == -1)
+	{
+		fprintf(stderr, "Error: Could not open file\n");
+		return (-1);
+	}
 
-    // Check if file opening failed
-    if (fd == -1) {
-        return -1;
-    }
+	write_status = write(file_descriptor, text_content, content_length);
+	if (write_status == -1)
+	{
+		fprintf(stderr, "Error: Could not write to file\n");
+		close(file_descriptor);
+		return (-1);
+	}
 
-    // Write the text to the file
-    wrCount = write(fd, text_content, len);
-
-    // Check if the write operation failed
-    if (wrCount == -1) {
-        close(fd);
-        return -1;
-    }
-
-    // Close the file and return 1
-    close(fd);
-    return 1;
+	close(file_descriptor);
+	return (1);
 }
-
